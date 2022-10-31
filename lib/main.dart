9474687c7b59339bacea2d8c1ed77a1139aefc06
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-10-31 19:33:58 +0100

    XylophoneButtons play sound now

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
-  XylophoneApp({super.key});
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({super.key});
 
-  AudioCache note1 = AudioCache(prefix: 'note1.wav');
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
 
-  void playNote(AudioCache note1) {
-    AudioPlayer
+class _XylophoneAppState extends State<XylophoneApp> {
+  AudioPlayer note1 = AudioPlayer();
+
+  void playSound(String note) async {
+    ByteData bytes = await rootBundle.load('assets/$note.wav');
+    Uint8List soundbytes =
+        bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+    int result = await note1.playBytes(soundbytes);
+    if (result == 1) {
+      //play success
+      print("Sound playing successful.");
+    } else {
+      print("Error while playing sound.");
+    }
   }
 
   @override
@@ -24,43 +39,57 @@ class XylophoneApp extends StatelessWidget {
             children: [
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note1');
+                  },
                   color: Colors.red,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note2');
+                  },
                   color: Colors.orange,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note3');
+                  },
                   color: Colors.yellow,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note4');
+                  },
                   color: Colors.green,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note5');
+                  },
                   color: Colors.teal,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note6');
+                  },
                   color: Colors.blue,
                 ),
               ),
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    playSound('note7');
+                  },
                   color: Colors.purple,
                 ),
               ),
