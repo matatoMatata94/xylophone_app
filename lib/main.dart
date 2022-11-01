6ddc5676bc05a7d96d3898083b6b95d1f5eda6ff
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-11-01 21:48:51 +0100

    Created a xylophoneButton

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class XylophoneApp extends StatefulWidget {
 class _XylophoneAppState extends State<XylophoneApp> {
   AudioPlayer note1 = AudioPlayer();
 
-  void playSound(String note) async {
-    ByteData bytes = await rootBundle.load('assets/$note.wav');
+  void playSound(int noteNumber) async {
+    ByteData bytes = await rootBundle.load('assets/note$noteNumber.wav');
     Uint8List soundbytes =
         bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
     int result = await note1.playBytes(soundbytes);
@@ -25,6 +25,17 @@ class _XylophoneAppState extends State<XylophoneApp> {
     } else {
       print("Error while playing sound.");
     }
+  }
+
+  Expanded xylophoneButton({required int noteNumber, required Color color}) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () async {
+          playSound(noteNumber);
+        },
+        color: color,
+      ),
+    );
   }
 
   @override
@@ -37,62 +48,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note1');
-                  },
-                  color: Colors.red,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note2');
-                  },
-                  color: Colors.orange,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note3');
-                  },
-                  color: Colors.yellow,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note4');
-                  },
-                  color: Colors.green,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note5');
-                  },
-                  color: Colors.teal,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note6');
-                  },
-                  color: Colors.blue,
-                ),
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () async {
-                    playSound('note7');
-                  },
-                  color: Colors.purple,
-                ),
-              ),
+              xylophoneButton(noteNumber: 1, color: Colors.red),
+              xylophoneButton(noteNumber: 2, color: Colors.orange),
+              xylophoneButton(noteNumber: 3, color: Colors.yellow),
+              xylophoneButton(noteNumber: 4, color: Colors.green),
+              xylophoneButton(noteNumber: 5, color: Colors.teal),
+              xylophoneButton(noteNumber: 6, color: Colors.blue),
+              xylophoneButton(noteNumber: 7, color: Colors.purple),
             ],
           ),
         ),
